6911a46d12ab1c3b9dacb12006306078742a43a4
Mycodee <code.jsehr@gmail.com>
2022-09-11 18:21:24 +0200

    Added Database Configuration && Configuration entry in design

--- src/lib/configuration.rs
+++ src/lib/configuration.rs
@@ -8,12 +8,22 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Configuration {
     pub discord: DiscordConfiguration,
+    pub database: DatabaseConfiguration,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DiscordConfiguration {
     pub app_id: String,
     pub token: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DatabaseConfiguration {
+    pub host: String,
+    pub port: Option<u32>,
+    pub user: Option<String>,
+    pub password: Option<String>,
+    pub database: Option<String>,
 }
 
 pub fn load_config(path: Option<&str>) -> eyre::Result<Configuration> {
